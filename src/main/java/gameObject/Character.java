7543f5d9dac1f0f409727360cd0784c908dcee6b
Max <max.hammond21@gmail.com>
2022-10-15 20:40:02 +1100

    added files

--- src/main/java/gameObject/Character.java
+++ src/main/java/gameObject/Character.java
@@ -3,11 +3,22 @@ package gameObject;
 import tileMap.*;
 import gremlins.*;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.data.JSONObject;
+import processing.data.JSONArray;
+
+
 public class Character {
 
-    private Tile[][] currentMap;
+    private PImage character;
+    protected Tile[][] currentMap;
 
     public Character(Tile[][] currentMap) {
         this.currentMap = currentMap;
     }
+
+    public Tile[][] drawCharacter() {
+    }
 }
+
